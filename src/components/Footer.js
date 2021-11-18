e2a0782c814b1f535880c51b1fb42eb38864579e
AndreaAguado <andrea95am@gmail.com>
2021-11-18 22:21:48 +0100

    add active class to navlink

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,28 @@
 import '../styles/components/Footer.scss';
+import { NavLink } from 'react-router-dom';
 const Footer = () => {
     return (
-        <footer class="footer">
+        <footer className="footer">
             <nav>
                 <ul>
-                    <li class="footer__menu-item">
-                        <a class="footer__menu-link" href="#/">A jugar</a>
+                    <li className="footer__menu-item">
+                        <NavLink className={(navData) => { return (navData.isActive ? "footer__menu-link active" : "footer__menu-link ") }} to="/">
+                            A jugar
+                        </NavLink>
                     </li>
-                    <li class="footer__menu-item">
-                        <a class="footer__menu-link active" href="#/instructions">¿Cómo se juega?</a>
+                    <li className="footer__menu-item">
+                        <NavLink className={(navData) => { return (navData.isActive ? "footer__menu-link active" : "footer__menu-link ") }} to="/instructions">
+                            ¿Cómo se juega?
+                        </NavLink>
                     </li>
-                    <li class="footer__menu-item">
-                        <a class="footer__menu-link" href="#/options">Más opciones</a>
+                    <li className="footer__menu-item">
+                        <NavLink className={(navData) => { return (navData.isActive ? "footer__menu-link active" : "footer__menu-link ") }} to="/options">
+                            Más opciones
+                        </NavLink>
                     </li>
                 </ul>
             </nav>
-            <small class="footer__copy">© Adalab</small>
+            <small className="footer__copy">© Adalab</small>
         </footer>
     )
 }
