0d7910b5af26b26e88e4b15d7ff5c141c4ea1b10
AndreaAguado <andrea95am@gmail.com>
2021-11-19 13:59:44 +0100

    add form to choose word to guess

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,8 +3,25 @@ import Dummy from "./Dummy";
 
 const Options = (props) => {
     return (
-        <section className="options_page">
-            <p className="options">Estas son las opciones del juego</p>
+        <section className="options">
+            <div className="options_container">
+                <h2 className="options_title">Estas son las opciones del juego
+                </h2>
+                <form>
+                    <label className="title" htmlFor="word">
+                        Escribe aquí la palabra que hay que adivinar:
+                    </label>
+                    <input
+                        autofocus
+                        autocomplete="off"
+                        class="form__input"
+                        maxlength="15"
+                        type="text"
+                        id="word"
+                        name="word"
+                    />
+                </form>
+            </div>
             <Dummy calcErrors={props.calcErrors}></Dummy>
         </section>
 
