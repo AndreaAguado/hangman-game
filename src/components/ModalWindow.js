d7a0a04bda4bba63498d46f5d0a42ae8998076c8
AndreaAguado <andrea95am@gmail.com>
2021-11-26 19:51:29 +0100

    add function to get error/win message

--- src/components/ModalWindow.js
+++ src/components/ModalWindow.js
@@ -3,12 +3,20 @@ import { Link } from 'react-router-dom';
 
 
 function ModalWindow(props) {
+    const gameOverMessage = () => {
+        if (props.calcErrors === 13) {
+            return (<p>Has perdido :(</p>)
+        }
+        else {
+            return (<p>Has ganado!! :D</p>)
+        }
+    }
     return (
         <div className='modal'>
             <div className='modal__dialog'>
                 <div className='modal__content'>
                     <header className='modal__header'>
-                        <h2 className='modal__title'>{props.title}</h2>
+                        <h2 className='modal__title'>{gameOverMessage()}</h2>
                         <Link to='/'>
                             <span className='modal__close icon fas fa-times'></span>
                         </Link>
